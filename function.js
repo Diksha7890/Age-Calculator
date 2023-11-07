9a975cc327735f4fcf1b7789793c19967f35f783
Diksha <dikshabhatia1705@gmail.com>
2023-11-07 14:58:02 +0530

    some changes in funtions

--- function.js
+++ function.js
@@ -12,30 +12,49 @@ function submit() {
   let curYear = currntdate.getFullYear();
   console.log(new Date().getMonth());
 
-  // Validating  the data
-  if (Bdydate == "" || Bdymonth == "" || Bdyyear == "") {
-    showmsg("please provide  complete details");
-  } else if (isNaN(Bdydate)) {
+  if (isNaN(Bdydate)) {
     showmsg("please enter valid date");
-  } else if (Bdydate <= 0) {
+    return false;
+  }
+  if (Bdydate <= 0) {
     showmsg("Date cannot be zero or less than zero");
-  } else if (Bdydate > months[Bdymonth - 1]) {
+    return false;
+  }
+  if (Bdydate == "" || Bdydate > months[Bdymonth - 1]) {
     showmsg("Please provide valid date with respect to month");
-  } else if (isNaN(Bdymonth)) {
+    return false;
+  }
+  if (isNaN(Bdymonth)) {
     showmsg("please enter valid month");
-  } else if (Bdymonth <= 0) {
+    return false;
+  }
+  if (Bdymonth <= 0) {
     showmsg("Month cannot be zero or less than zero");
-  } else if (Bdymonth > 12) {
+    return false;
+  }
+  if (Bdymonth == "" || Bdymonth > 12) {
     showmsg("Please provide month value in the range from 1 to 12");
-  } else if (isNaN(Bdyyear)) {
+    return false;
+  }
+  if (isNaN(Bdyyear)) {
     showmsg("please enter valid year");
-  } else if (Bdyyear <= 0) {
+    return false;
+  }
+  if (Bdyyear <= 0) {
     showmsg("year cannot be zero or less than zero");
-  } else if (Bdyyear > curYear) {
+    return false;
+  }
+  if (Bdyyear > curYear) {
     showmsg("Please provide valid year");
-  } else if (Bdyyear.length < 4) {
+    return false;
+  }
+  if (Bdyyear == "" || Bdyyear.length < 4) {
     showmsg("Please provide year in the format YYYY");
-  } else if (curDate < Bdydate) {
+    return false;
+  }
+
+
+  if (curDate < Bdydate) {
     curDate += months[Bdymonth - 1];
     curMonth--;
     if (curMonth < Bdymonth) {
@@ -45,16 +64,14 @@ function submit() {
   } else if (curMonth < Bdymonth) {
     curMonth += 12;
     curYear--;
-  } else {
-    curDate -= Bdydate;
-    curMonth -= Bdymonth;
-    curYear -= Bdyyear;
-    showmsg(
-      `Your age is ${curYear} years ${curMonth} months and ${curDate} days`
-    );
   }
+  curDate -= Bdydate;
+  curMonth -= Bdymonth;
+  curYear -= Bdyyear;
+  showmsg(
+    `Your age is ${curYear} years ${curMonth} months and ${curDate} days`
+  );
 }
-// showing current age on ui
 function showmsg(msg) {
   let msgElem = document.querySelector(".msg");
   msgElem.innerHTML = msg;
